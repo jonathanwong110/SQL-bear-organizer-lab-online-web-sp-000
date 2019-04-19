9f0f8a4a4371002222128b36be51ea5827c5cdab
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-19 02:29:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 1, "M", "brown", "calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 2, "M", "brown", "calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 3, "M", "brown", "calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 4, "M", "brown", "calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 5, "F", "brown", "calm", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 6, "F", "brown", "calm", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 7, "F", "brown", "calm", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 8, "F", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 20, "M", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 10, "M", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 6, "M", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 19, "M", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 13, "F", "brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "F", "brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 6, "F", "brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 20, "F", "brown", "calm", 1);

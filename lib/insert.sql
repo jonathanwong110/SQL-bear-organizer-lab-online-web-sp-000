73bbc038f5aa1c1c634985076db6d2fe8e9a79b5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-19 01:59:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (1, "A", 1, "M", "brown", "calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (2, "B", 2, "M", "brown", "calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (3, "C", 1, "M", "brown", "calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (4, "D", 2, "M", "brown", "calm", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (5, "E", 1, "F", "brown", "calm", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (6, "F", 2, "F", "brown", "calm", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (7, "G", 1, "F", "brown", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 1, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 2, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 1, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 2, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 1, "F", "brown", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "F", "brown", "calm", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 1, "F", "brown", "calm", false);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, "H", 2, "F", "brown", "calm", false);

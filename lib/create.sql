f1ac43bcdddf48f387b671e2fa350e798edbaf6e
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-19 01:48:40 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+sqlite> CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament, TEXT, alive BOOLEAN);

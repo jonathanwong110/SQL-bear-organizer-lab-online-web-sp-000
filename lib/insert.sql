2ac28cc15b7e237f0fe1e0b63d999eab0f21eb81
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-19 02:04:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 1, "M", "brown", "calm", true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 2, "M", "brown", "calm", true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 1, "M", "brown", "calm", true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 2, "M", "brown", "calm", true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 1, "F", "brown", "calm", false);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 2, "F", "brown", "calm", false);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 1, "F", "brown", "calm", false);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "Sunny", 2, "F", "brown", "calm", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (1, "Mr. Chocolate", 1, "M", "brown", "calm", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (2, "Rowdy", 2, "M", "brown", "calm", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (3, "Tabitha", 1, "M", "brown", "calm", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (4, "Sergeant Brown", 2, "M", "brown", "calm", true);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (5, "Melissa", 1, "F", "brown", "calm", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (6, "Grinch", 2, "F", "brown", "calm", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (7, "Wendy", 1, "F", "brown", "calm", false);
+INSERT INTO bears (id, name, age, gender, color, temperament, true) VALUES (NULL, "Sunny", 2, "F", "brown", "calm", false);

bac7087427d70f6a3422ccbc98dab1880089bb4c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-19 02:30:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 20, "M", "brown", "calm", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 10, "M", "brown", "calm", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 6, "M", "brown", "calm", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 19, "M", "brown", "calm", 1);

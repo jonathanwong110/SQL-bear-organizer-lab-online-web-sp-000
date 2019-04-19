140aa31e1cf6fe65aa8df9b8b01610a6e834e7e7
Jonathan Wong <jonathan.wong110@gmail.com>
2019-04-19 02:31:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3,"
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 6, "F", "brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 6, "F", "Blue", "naive", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 20, "F", "brown", "calm", 1);
